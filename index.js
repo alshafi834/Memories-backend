ddb0c3ef94ff201037332c7c29b5dec8b83262a5
alshafi834 <alshafi834@gmail.com>
2020-12-29 20:48:18 +0100

    api ready

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
-const CONNECTION_URL = "mongodb+srv://alshafi834:158893SDnm_)@cluster0.asrk4.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const CONNECTION_URL =
+  "mongodb+srv://alshafi834:158893SDnm_)@cluster0.asrk4.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
-const app = express()
+const app = express();
 
-app.use('/posts', postRoutes)
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(express.json({limit: "30mb", extended: true}))
-app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
 
-app.use(cors())
+app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
-.catch((error) => console.log(error.message)); 
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
